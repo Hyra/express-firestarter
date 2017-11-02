7606c71f8cbe2be03ef5c97854bfb8e19282d929
Hyra <stef@mindthecode.com>
2017-11-02 07:29:44 +0100

    Process env

--- worker.js
+++ worker.js
@@ -24,7 +24,7 @@ require('dotenv').config();
 //     done()
 // });
 
-if (production.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
     var socket = require('socket.io-client');
 } else {
     var socket = require('socket.io-client')('http://localhost:3000');
