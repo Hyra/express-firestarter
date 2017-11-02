1c4cdf02904b2b0f1a0d0121009132b2a4427334
Hyra <stef@mindthecode.com>
2017-11-02 09:23:53 +0100

    Worker typo

--- sockets.js
+++ sockets.js
@@ -2,7 +2,7 @@ exports = module.exports = function(io) {
     io.on('connection', function(socket) {
         console.log('SOCKET.JS', 'A user connected')
         socket.on('sample-message', function (data) {
-            console.log('RECEVIED FROM WORKER', data)
+            console.log('RECEIVED FROM WORKER', data)
             io.of('/' + data.room).emit('newpage', {
                 title: Math.ceil(Math.random() * 1020191)
             })
