42b8e66af35aaf46024f21b4f2e93cb5d236f342
Hyra <stef@mindthecode.com>
2017-11-15 15:49:24 +0100

    Moved JS to ‘src’ folder

---
+++ src/components/example.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+        <h1>Heya</h1>
+        <ul>
+            <li v-for="page in pages">
+                {{ page.title }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            pages: []
+        }
+    },
+    props: {
+        slug: {
+            type: String
+        }
+    },
+    socket: {
+        events: {
+            // https://github.com/icebob/vue-websocket
+            // Similar as this.$socket.on("changed", (msg) => { ... }); 
+            // If you set `prefix` to `/counter/`, the event name will be `/counter/changed` 
+            // 
+            newpage(msg) {
+                console.log("Something changed: ",  msg);
+                console.log(msg);
+            },
+            connect() {
+                    console.log("Websocket connected to " + this.$socket.nsp);
+                },
+        }
+    },
+    mounted () {
+        let that = this
+        console.log('mounted')
+    }
+}
+</script>
+
+<style scoped lang="scss">
+.ha {
+    color: green
+    span {
+        color: red
+    }
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -2,16 +2,19 @@ var Vue = process.env.NODE_ENV === 'production' ? require('vue/dist/vue.min.js')
 
 Vue.component('example', require('./components/example.vue'));
 
+import VueWebsocket from "vue-websocket";
+
+if(process.env.NODE_ENV === 'production') {
+    var socket = io.connect('https://' + location.hostname + '/', { reconnect: true, transports: ['websocket'], path: '/socket.io' })
+    Vue.use(VueWebsocket, socket)
+} else {
+    Vue.use(VueWebsocket, 'http://localhost:3000')
+}
+
 const app = new Vue({
   el: '#app'
 })
 
-if(process.env.NODE_ENV === 'production') {
-    var socket = io.connect('https://' + location.hostname + '/', { reconnect: true, transports: ['websocket'], path: '/socket.io' })
-} else {
-    var socket = io.connect('http://localhost:3000');
-}
-
-socket.on('newpage', function (data) {
-  console.log(data);
-});
+// socket.on('newpage', function (data) {
+//   console.log(data);
+// });
